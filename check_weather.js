73048f7f46f1ae01f352fcd8d47781441d882462
Saipuneethg <saipuneeth2005@gmail.com>
2026-08-04 21:13:12 +0530

    Clean up root directory and remove maintenance scripts for production deployment

--- check_weather.js
+++
@@ -1,6 +0,0 @@
-async function check() {
-  const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Guntur&appid=523250a69a2145b79e951728261806&units=metric')
-  const text = await res.text()
-  console.log(res.status, text)
-}
-check()
